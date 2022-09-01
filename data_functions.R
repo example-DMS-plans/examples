46abbf55b3ff9f47b84e5bd53d548c05e0012274
Hao Ye <lhopitalified@gmail.com>
2022-09-01 14:33:16 -0400

    fix authentication

--- data_functions.R
+++ data_functions.R
@@ -14,9 +14,8 @@ get_data_url <- function()
 download_raw_data <- function(DATA_URL = get_data_url(), 
                               file_out = "raw_data.RDS")
 {
-    gs4_auth(path = rawToChar(get_gs4_token()), 
-             email = TRUE, 
-             scopes = "https://www.googleapis.com/auth/spreadsheets.readonly")
+    gs4_deauth()
+    gs4_auth(token = get_gs4_token())
     dat <- read_sheet(DATA_URL, col_types = "c")
     saveRDS(dat, file_out)
 }
@@ -26,18 +25,29 @@ process_raw_data <- function(file_in = "raw_data.RDS")
     dat <- readRDS(file_in)
 }
 
-get_gs4_token <- function()
+decrypt_gs4_token <- function()
 {
-    token <- NULL
+    token_BIN <- NULL
     if (gargle:::secret_can_decrypt("gargle"))
     {
         path <- "inst/secret/gs4-auth.token"
         raw <- readBin(path, "raw", file.size(path))
-        token <- sodium::data_decrypt(bin = raw, 
+        token_BIN <- sodium::data_decrypt(bin = raw, 
                                       key = gargle:::secret_pw_get("gargle"), 
                                       nonce = gargle:::secret_nonce())
     }
     
-    invisible(token)
+    invisible(token_BIN)
 }
 
+write_gs4_token <- function(token_BIN, file_out = "token.RDS")
+{
+    writeBin(token_BIN, file_out)
+}
+
+get_gs4_token <- function()
+{
+    token_BIN <- decrypt_gs4_token()
+    write_gs4_token(token_BIN)
+    readRDS("token.RDS")
+}
